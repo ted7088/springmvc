7274d2a60cd8fa7d2673e86a9e1024e49621de22
master <ted3131@diquest.com>
2023-10-13 20:03:49 +0900

    commit

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -15,6 +15,7 @@ public class ResponseViewController {
         return mav;
     }
 
+    //모델로 넘기는 방법
     @RequestMapping("/response-view-v2")
     public String responseViewV2(Model model){
         model.addAttribute("data", "hello");
@@ -22,6 +23,7 @@ public class ResponseViewController {
         return "response/hello";
     }
 
+    //모델로 넘기는 방법 근데 jsp 즉 스트링을 넘기지 않아 불명확
     @RequestMapping("/response/hello")
     public void responseViewV3(Model model) {
         model.addAttribute("data", "hello!!");
